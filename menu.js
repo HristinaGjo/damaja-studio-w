61831d2909734a194565f9310e4c04b10a3096ca
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-09-17 19:33:26 +0200

    added menu toggale for mobile

---
+++ menu.js
@@ -0,0 +1,23 @@
+const menuToggle = document.getElementById('menuToggle');
+const overlay = document.getElementById('overlay');
+
+// Toggle overlay menu on click
+menuToggle.addEventListener('click', () => {
+    // Check if the overlay is active (open)
+    if (overlay.classList.contains('active')) {
+        // Close the menu and go to homepage
+        overlay.classList.remove('active');
+        menuToggle.textContent = 'menu';  // Change the button back to 'Menu'
+        menuToggle.style.color = '#333333';  // Set text color to black
+        
+        // Redirect to homepage (change 'index.html' or '/' to your home URL if needed)
+        window.location.href = '/';  // You can adjust the URL based on your homepage route
+
+    } else {
+        // Open the menu
+        overlay.classList.add('active');
+        menuToggle.textContent = 'close';  // Change the button text to 'Close'
+        menuToggle.style.color = '#f2f2f2';  // Set text color to white
+        menuToggle.style.fontWeight = '200'; 
+    }
+});
