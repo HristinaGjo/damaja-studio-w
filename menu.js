cce20e698c93856cdf8b287b0ec773342b34c156
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-10-04 10:17:01 +0200

    hide logo on mobile when menu is open to test

--- menu.js
+++ menu.js
@@ -1,5 +1,6 @@
 const menuToggle = document.getElementById('menuToggle');
 const overlay = document.getElementById('overlay');
+const logo = document.getElementById('logo')
 
 // Toggle overlay menu on click
 menuToggle.addEventListener('click', () => {
@@ -9,6 +10,8 @@ menuToggle.addEventListener('click', () => {
         overlay.classList.remove('active');
         menuToggle.textContent = 'menu';  // Change the button back to 'Menu'
         menuToggle.style.color = '#333';  // Set text color to black
+
+        logo.style.display = 'block';
         
         // Redirect to homepage (change 'index.html' or '/' to your home URL if needed)
         window.location.href = "https://hristinagjo.github.io/damaja-studio/";  // You can adjust the URL based on your homepage route
@@ -19,6 +22,8 @@ menuToggle.addEventListener('click', () => {
         menuToggle.textContent = 'CLOSE';  // Change the button text to 'Close'
         menuToggle.style.color = '#f2f2f2';  // Set text color to white
         /*menuToggle.style.fontWeight = '200'; */
+
+        logo.style.display = 'none'
     }
 });
 
